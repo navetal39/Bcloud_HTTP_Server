fb932c5d15ea775bdc7e1017a475c471715b625a
Nave <navetal@gmail.com>
2015-02-22 11:15:01 +0200

    Started HTTP server extra seperation

--- HTTP_Server_v0.py
+++ HTTP_Server_v0.py
@@ -25,24 +25,6 @@ from Main_Server_Com import Server
 NUM_OF_THREADS = 20
 SIZE_OF_QUEUE = 40
 
-## Pahts: ##
-### Error paths: ###
-ERROR_404_PATH = "Pages/Error404.htm"
-ERROR_500_PATH = "Pages/Error500.htm"
-NO_NAME_ERROR_PATH = "Pages/ErrorNoName.htm"
-EMPTY_FOLDER_ERROR_PATH = "Pages/ErrorEmptyFolder.htm"
-NAME_IN_USE_ERROR_PATH = "Pages/ErrorNameInUse.htm"
-### General paths: ###
-LAST_UPDATE_PLUS_PATH = "Pages/LastUpdatePlus.htm"
-HE_GAVE_UP_PATH = "Pages/HeGaveUp.htm"
-HE_SAID_YES_PATH = "Pages/ThanksFor.htm"
-SIGN_UP_APPROVAL_PATH = "Pages/SignUpApproval.htm"
-
-##Server-Server Communication: ##
-SERVER_COM_IP="127.0.0.1"
-SERVER_COM_PORT=3417
-main_server=Server(SERVER_COM_IP, SERVER_COM_PORT)
-
 ## Other usefull stuff: ##
 STATUS_LINES = {"200": "HTTP/1.1 200 OK\r\n", "404": "HTTP/1.1 404 NOT FOUND\r\n", "301": "HTTP/1.1 301 Moved Permanently\r\n",
                 "302":"HTTP/1.1 302 Found\r\n", "500": "HTTP/1.1 500 Internal Server Error"}
@@ -165,38 +147,7 @@ def do_work():
                     path = parsed_url.path.lstrip('/')
                     params = parsed_url.query
                     if params: # Download
-                        key, value = params.split("=") # Because there is ONLY one parameter, for sure.
-                        if key == "username": # Download - first part.
-                            name = value # For the second part.
-                            stat, data = main_server.get_last_update(name)
-                            if stat == "NNM":
-                                path = NO_NAME_ERROR_PATH
-                                status = "200"
-                            elif stat == "EMP":
-                                path = EMPTY_FOLDER_ERROR_PATH
-                                status = "200"
-                            elif stat == "SCS":
-                                path = LAST_UPDATE_PLUS_PATH # Add last update...!
-                                status = "200"
-                            else:
-                                raise
-                            
-                        elif key == "is_approved": # Download - second part.
-                            if value == "YES": # Partial implementetion, need to add distinguishing things with URI, etc.!
-                                path = HE_SAID_YES_PATH
-                                status = "200"
-                                folder_flag = True
-                            elif value == "NO":
-                                path = HE_GAVE_UP_PATH
-                                status = "200"
-                            else: # If the user decides to try to be funny and put something else in the url rather than "YES/NO" thinking that he may crash our server by doing so...
-                                status = "404"
-                                path = ERROR_404_PATH
-                                
-                        else: # Same shit again... If the user decides to try to be funny and put something else in the url rather than "username/is_approved" thinking that he may crash our server by doing so...
-                            status = "404"
-                            path = ERROR_404_PATH
-                    
+                        status, path = extra.download(params)
                     else: # Normal 'GET'
                         if path == "favicon.ico":
                             path = "Pages/favicon.ico"
