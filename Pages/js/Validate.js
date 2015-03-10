a82756ee89680ce9d73dc6c488d47aec61aa9e99
Tamir SH <tamir2341@gmail.com>
2015-03-10 17:17:50 +0200

    Some changes in HTML pages... In the middle

--- Pages/js/Validate.js
+++ Pages/js/Validate.js
@@ -1,14 +1,21 @@
-function checkAndSend() {
+function validation(str) {
 	var forbidden = ['|','\\', '/', ':', '*', '?', '"', '<', '>'];
-	var username = document.getElementById('username').value;
-	var valid = true;
 	
 	for (var i=0; i < forbidden.length; i++) {
-		if (username.indexOf(forbidden[i]) >= 0) { //If this is true, so the username is invalid.
-			valid = false;
-			break;
+		if (str.indexOf(forbidden[i]) >= 0) { //If this is true, so the string is invalid.
+			return false;
 		}
 	}
+	
+	return true;
+}
+
+
+/* For the FolderDownload page: */
+function checkAndSend1() {
+	var username = document.getElementById('username').value;
+	
+	var valid = validation(username);
 	
 	if (valid) {
 		var url = "http://localhost:8080/Downloading?username="+username;
@@ -17,3 +24,25 @@ function checkAndSend() {
 		alert("The username is invalid, please enter a different one. These characters are forbidden: '|','\\', '/', ':', '*', '?', '\"', '<', '>'");
 	}
 }
+
+
+/* For the SignUp page: */
+function checkAndSend2() {
+	var username = document.getElementById('username').value;
+	var password = document.getElementById('password').value;
+	
+	var valid = validation(username) && validation(password);
+	
+	if (valid) {
+		var x = "A";
+		//var url = "http://localhost:8080/SignUpApproval.htm";
+		//window.open(url, "_self");
+	} else {
+		alert("The username is invalid, please enter a different one. These characters are forbidden: '|','\\', '/', ':', '*', '?', '\"', '<', '>'");
+	}
+}
+
+/*
+Exciting. Satisfying. Period.
+.
+*/
