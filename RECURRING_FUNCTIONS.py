702e1bd9069a146551867308b41d50a601f12e51
Tamir SH <tamir2341@gmail.com>
2015-04-14 19:55:19 +0300

    For The Record convention fixes

--- RECURRING_FUNCTIONS.py
+++ RECURRING_FUNCTIONS.py
@@ -11,7 +11,7 @@ def file_recv(sock, count = 0):
     ''' This method is for reciving large files.
     '''
     response = sock.recv(5000)
-    print 'recived '+response # for the record...
+    print 'recived '+response # -For The Record-
     flag, str_size = response.split('|')
     try:
         if flag != 'SIZ':
@@ -19,16 +19,16 @@ def file_recv(sock, count = 0):
         size = int(str_size)
     except:
         if count < 3: #Just making sure that it won't attemt endlessly
-            print 'try again' # for the record...
+            print 'try again' # -For The Record-
             sock.send('NAK|'+response)
             final_response = file_recv(sock, count+1)
         else:
             final_response = 'WTF'
     else:
         sock.send('ACK|'+response)
-        print 'go for it' # for the record...
+        print 'go for it' # -For The Record-
         final_response = sock.recv(size)
-        print 'recived file' # for the record...
+        print 'recived file' # -For The Record-
     finally:
         return final_response
 
@@ -37,25 +37,26 @@ def file_send(sock, mess):
     '''
     size = len(mess)
     size_message = 'SIZ|{}'.format(size)
-    print 'sending '+size_message # for the record...
+    print 'sending '+size_message # -For The Record-
     sock.send('SIZ|{}'.format(size))
     response = sock.recv(5000)
-    print 'recived '+response # for the record...
+    print 'recived '+response # -For The Record-
     response_parts = response.split('|')
     flag = response_parts[0]; response_parts.remove(flag)
     if response_parts == size_message.split('|'):
         if flag == 'NAK':
-            print 'trying again' # for the record...
+            print 'trying again' # -For The Record-
             file_send(sock, mess)
             return
         elif flag == 'ACK':
             sock.send(mess)
-            print 'sent mess' # for the record...
+            print 'sent mess' # -For The Record-
         else: #Just so there'll be an else...
             pass
     else:
         print response_parts
         print size_message.split('|')
+
 
 
 '''
