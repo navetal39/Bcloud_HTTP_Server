bab07e5c56026bf2aeac8b657ecb996ab19a27ef
Tamir SH <tamir2341@gmail.com>
2015-04-17 15:11:57 +0300

    got script out of LUP page

---
+++ Pages/js/download_help.js
@@ -0,0 +1,8 @@
+var sIP = location.host; /* <<<<<< This is the server's IP address. <<<<<< */
+if (sIP == "") { sIP = "localhost"; }
+
+function get_last_update() {
+	var username = document.getElementById('username').innerHTML;
+	var url = "http://" + sIP + "/Downloading?username=" + username + "&is_approved=YES";
+	window.open(url, "_self");
+}
