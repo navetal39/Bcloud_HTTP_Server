4062dcdc8b8ae25aac32a1abad56eb7d0b0811be
Tamir SH <tamir2341@gmail.com>
2015-04-21 10:30:59 +0300

    docs :+1:

--- Main_Server_Com.py
+++ Main_Server_Com.py
@@ -18,19 +18,29 @@ class Server(object): # The HTTP server sees is as a server, the main server see
         self.MAIN_SOCKET = socket.socket()
     
     def __str__(self):
+		''' Used for debug.
+		'''
         return "ip: {ip}; port: {port}".format(ip=self.MAIN_IP, port=self.MAIN_PORT)
     
     def __repr__(self):
+		''' Used for debug.
+		'''
         return "################\nThe main server is (or at least should be) listening on:\nIP address: {ip}\nTCP port: {port}\n################".format(ip=self.MAIN_IP, port=self.MAIN_PORT)
 
     def connect(self):
+		''' Connects to the main server. Used for convenience.
+		'''
         self.MAIN_SOCKET = socket.socket()
         self.MAIN_SOCKET.connect((self.MAIN_IP, self.MAIN_PORT))
 
     def disconnect(self):
+		''' Disconnects (closes the socket) from the main server. Used for convenience.
+		'''
         self.MAIN_SOCKET.close()
     
     def create_user(self, name, pw):
+		''' This method asks the main server to register a new user. Then it return a flag implying whether it worked or not. 
+		'''
         message = "REG|{n}|{p}".format(n=name, p=pw)
         self.connect()
         self.MAIN_SOCKET.send(message)
@@ -38,12 +48,14 @@ class Server(object): # The HTTP server sees is as a server, the main server see
         self.disconnect()
         resp_parts = resp.split('|')
         flag = resp_parts[0]; resp_parts.remove(flag)
-        if resp_parts != message.split('|'):
+        if resp_parts != message.split('|'): # The response is suposed to be a flag and then the request sent. If it's not - something is wrong (500).
             return "WTF"
         else:
             return flag
-        
+			
     def get_last_update(self, username, folder_name):
+		'''
+		'''
         message = "LUD|{}|{}".format(username, folder_name)
         self.connect()
         self.MAIN_SOCKET.send(message)
@@ -68,7 +80,7 @@ class Server(object): # The HTTP server sees is as a server, the main server see
         
     def get_folder(self, folder_name, count = 0):
         ''' Sends a request to get a specific folder. If it exists it should get a response
-            in the folowing format: "SCS|<DATA>". If it does not it should get "NNM".
+            in the flowing format: "SCS|<DATA>". If it does not it should get "NNM".
             In the case the folder does not exist this function should load the dedicated
             HTML file and send it instead.
         '''
